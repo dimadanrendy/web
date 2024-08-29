a3cafd74e1d09d8e2b7b94066ef1cdbf4fb419e2
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2024-08-29 13:11:12 +0700

    updated

--- src/app/(presentation)/layout.tsx
+++ src/app/(presentation)/layout.tsx
@@ -4,7 +4,7 @@ import PageFooterInformation from "@/components/components-ui/footer-information
 import PageHeader from "@/components/components-ui/header";
 import PageTopInformation from "@/components/components-ui/top-information";
 import type { Metadata } from "next";
-import NextNProgress from 'nextjs-progressbar';
+
 
 
 export default function RootLayout({
