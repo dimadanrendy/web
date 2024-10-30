0d42e7705d81fd923486fd232324c3bb44ca582c
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2024-10-30 14:57:31 +0700

    edithuf

--- src/features/management/auth/useLogOutAuth.ts
+++ src/features/management/auth/useLogOutAuth.ts
@@ -1,24 +1,35 @@
+// "use server";
+// import axiosInstance from "@/tools/axiosInstance";
+// import { cookies } from "next/headers";
+
+// export const logout = async () => {
+//   const cookieStore = await cookies();
+//   const token = cookieStore.get("X_ACCESS_TOKEN")?.value ?? "";
+//   if (!token) {
+//     return false;
+//   }
+//   (await cookies()).delete("X_ACCESS_TOKEN");
+//   try {
+//     await axiosInstance.delete(`/auth`, {
+//       headers: { Authorization: `Bearer ${token}` },
+//     });
+//     cookieStore.delete("X_ACCESS_TOKEN");
+//     cookieStore.delete("X_REFRESH_TOKEN");
+//     return true;
+//   } catch (error) {
+//     cookieStore.delete("X_ACCESS_TOKEN");
+//     cookieStore.delete("X_REFRESH_TOKEN");
+//     return true;
+//   }
+// };
+
 "use server";
-import axiosInstance from "@/tools/axiosInstance";
+
 import { cookies } from "next/headers";
 
-export const logout = async () => {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("X_ACCESS_TOKEN")?.value ?? "";
-  if (!token) {
-    return false;
-  }
-  (await cookies()).delete("X_ACCESS_TOKEN");
-  try {
-    await axiosInstance.delete(`/auth`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    cookieStore.delete("X_ACCESS_TOKEN");
-    cookieStore.delete("X_REFRESH_TOKEN");
-    return true;
-  } catch (error) {
-    cookieStore.delete("X_ACCESS_TOKEN");
-    cookieStore.delete("X_REFRESH_TOKEN");
-    return true;
-  }
-};
+// Menggunakan nama fungsi yang lebih deskriptif
+export async function deleteCookie() {
+  const cookieStore = cookies(); // Tidak perlu await
+  cookieStore.delete("X_ACCESS_TOKEN"); // Menghapus cookie dengan nama 'name'
+  cookieStore.delete("X_REFRESH_TOKEN");
+}
