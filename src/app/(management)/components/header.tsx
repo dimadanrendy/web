cb5e1a8652a21c6dc4baff7d2f7e87aff0472951
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2024-10-30 13:40:27 +0700

    edithuf

--- src/app/(management)/components/header.tsx
+++ src/app/(management)/components/header.tsx
@@ -50,6 +50,7 @@ export default function Header() {
         localStorage.removeItem('auth-storage')
         redirect('/login')
 
+
     };
     return (
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
