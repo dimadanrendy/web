e6e4835cf9bcb17298766ee593bbcda2ba130454
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2024-10-30 14:47:00 +0700

    edithuf

--- src/features/management/auth/useLogOutAuth.ts
+++ src/features/management/auth/useLogOutAuth.ts
@@ -1,8 +1,10 @@
+"use server";
 import axiosInstance from "@/tools/axiosInstance";
 import { cookies } from "next/headers";
 
 export const logout = async () => {
-  const token = cookies().get("X_ACCESS_TOKEN")?.value ?? "";
+  const cookieStore = await cookies();
+  const token = cookieStore.get("X_ACCESS_TOKEN")?.value ?? "";
   if (!token) {
     return false;
   }
@@ -11,13 +13,12 @@ export const logout = async () => {
     await axiosInstance.delete(`/auth`, {
       headers: { Authorization: `Bearer ${token}` },
     });
+    cookieStore.delete("X_ACCESS_TOKEN");
+    cookieStore.delete("X_REFRESH_TOKEN");
+    return true;
   } catch (error) {
-    console.error("Logout error:", error); // Log error jika ada
-  } finally {
-    // Selalu hapus token meskipun ada error
-    cookies().set("X_ACCESS_TOKEN", "", { expires: new Date(0) });
-    cookies().set("X_REFRESH_TOKEN", "", { expires: new Date(0) });
+    cookieStore.delete("X_ACCESS_TOKEN");
+    cookieStore.delete("X_REFRESH_TOKEN");
+    return true;
   }
-
-  return true;
 };
