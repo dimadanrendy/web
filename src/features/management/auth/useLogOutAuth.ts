756875b69212fc2e38c2a7678e9a87e10281254d
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2024-10-30 15:29:03 +0700

    edithuf

--- src/features/management/auth/useLogOutAuth.ts
+++ src/features/management/auth/useLogOutAuth.ts
@@ -1,29 +1,24 @@
 "use server";
-// import axiosInstance from "@/tools/axiosInstance";
+import axiosInstance from "@/tools/axiosInstance";
 import { cookies } from "next/headers";
 
-// export const logout = async () => {
-//   const cookieStore = cookies();
-//   const token = cookieStore.get("X_ACCESS_TOKEN")?.value ?? "";
-//   if (!token) {
-//     return false;
-//   }
+export const logout = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("X_ACCESS_TOKEN")?.value ?? "";
+  if (!token) {
+    return false;
+  }
 
-//   try {
-//     await axiosInstance.delete(`/auth`, {
-//       headers: { Authorization: `Bearer ${token}` },
-//     });
-//     cookieStore.delete("X_ACCESS_TOKEN");
-//     cookieStore.delete("X_REFRESH_TOKEN");
-//     return true;
-//   } catch (error) {
-//     cookieStore.delete("X_ACCESS_TOKEN");
-//     cookieStore.delete("X_REFRESH_TOKEN");
-//     return true;
-//   }
-// };
-
-export async function logout() {
-  // Destroy the session
-  cookies().set("X_ACCESS_TOKEN", "", { expires: new Date(0) });
-}
+  try {
+    await axiosInstance.delete(`/auth`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    cookieStore.delete("X_ACCESS_TOKEN");
+    cookieStore.delete("X_REFRESH_TOKEN");
+    return true;
+  } catch (error) {
+    cookieStore.delete("X_ACCESS_TOKEN");
+    cookieStore.delete("X_REFRESH_TOKEN");
+    return true;
+  }
+};
