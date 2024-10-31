33a32c565d35b3a08134896f3394a976ef801087
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2024-10-31 08:57:16 +0700

    edithuf

--- src/features/management/auth/useVerifyToken.ts
+++ src/features/management/auth/useVerifyToken.ts
@@ -17,36 +17,100 @@ export const verifyToken = async () => {
     return false;
   }
 
+  // if (id || token) {
+  //   const response = await axiosInstance.get(`/auth/${id}`, {
+  //     headers: { Authorization: `Bearer ${token}` },
+  //   });
+  //   console.log(response.data);
+  //   if (response.data.status === true) {
+  //     return response.data;
+  //   }
+
+  //   if (
+  //     response.data.status === false &&
+  //     response.data.message === "jwt expired"
+  //   ) {
+  //     const response = await axiosInstance.patch(
+  //       `/auth`,
+  //       {},
+  //       {
+  //         headers: { Authorization: `Bearer ${token}` },
+  //       }
+  //     );
+
+  //     cookies().set("X_ACCESS_TOKEN", response.data.access_token, {
+  //       httpOnly: true,
+  //       secure: process.env.NODE_ENV === "production",
+  //       maxAge: 1000 * 60 * 60 * 2,
+  //     });
+
+  //     cookies().set("X_REFRESH_TOKEN", response.data.refresh_token, {
+  //       httpOnly: true,
+  //       secure: process.env.NODE_ENV === "production",
+  //       maxAge: 1000 * 60 * 60 * 2,
+  //     });
+  //     return response.data;
+  //   }
+
+  //   if (
+  //     response.data.status === false &&
+  //     response.data.message === "jwt invalid"
+  //   ) {
+  //     await axiosInstance.delete(`/auth/${id}`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+  //     return false;
+  //   }
+  // }
+
   try {
     const response = await axiosInstance.get(`/auth/${id}`, {
       headers: { Authorization: `Bearer ${token}` },
     });
     return response.data;
-  } catch (error) {
-    const refreshToken = cookies().get("X_REFRESH_TOKEN")?.value ?? "";
-    if (!refreshToken) {
+  } catch (error: any) {
+    if (error.response.data.message === "jwt expired") {
+      const refreshToken = cookies().get("X_REFRESH_TOKEN")?.value ?? "";
+      if (!refreshToken) {
+        return false;
+      }
+
+      const response = await axiosInstance.patch(
+        `/auth`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${refreshToken}` },
+        }
+      );
+
+      cookies().set("X_ACCESS_TOKEN", response.data.access_token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 2,
+      });
+
+      cookies().set("X_REFRESH_TOKEN", response.data.refresh_token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 2,
+      });
+      return response.data;
+    } else {
+      if (tokenRefresh === "") {
+        cookies().delete("X_REFRESH_TOKEN");
+        cookies().delete("X_ACCESS_TOKEN");
+        return false;
+      }
+      const response = await axiosInstance.delete(`/auth`, {
+        headers: {
+          Authorization: `Bearer ${tokenRefresh}`,
+        },
+      });
+
+      console.log(response.data);
       return false;
     }
-
-    const response = await axiosInstance.patch(
-      `/auth`,
-      {},
-      {
-        headers: { Authorization: `Bearer ${refreshToken}` },
-      }
-    );
-
-    cookies().set("X_ACCESS_TOKEN", response.data.access_token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 1000 * 60 * 60 * 2,
-    });
-
-    cookies().set("X_REFRESH_TOKEN", response.data.refresh_token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 1000 * 60 * 60 * 2,
-    });
-    return response.data;
   }
 };
