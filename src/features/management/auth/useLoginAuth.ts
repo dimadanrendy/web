b88d6715f4ba1ad54a4de973b60838002afe3cac
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2025-01-08 23:00:52 +0700

    debuging

--- src/features/management/auth/useLoginAuth.ts
+++ src/features/management/auth/useLoginAuth.ts
@@ -3,8 +3,10 @@ import axiosInstance from "@/tools/axiosInstance";
 export const login = async (data: any) => {
   try {
     const response = await axiosInstance.post("/auth", data);
+    console.log(response);
     return response.data;
   } catch (error: any) {
+    console.log(error);
     return error.response.data.message;
   }
 };
