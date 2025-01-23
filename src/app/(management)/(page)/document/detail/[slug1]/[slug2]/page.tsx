d92665e8c63ac95381414767addca438ab738653
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2025-01-23 16:39:47 +0700

    feature: merubah add dokumen menjadi bisa upload zip dan rar

--- src/app/(management)/(page)/document/detail/[slug1]/[slug2]/page.tsx
+++ src/app/(management)/(page)/document/detail/[slug1]/[slug2]/page.tsx
@@ -184,12 +184,12 @@ export default function DetailDocument({ params }: { params: { slug1: String; sl
                     </div>
                     <div className="col-span-2">
                         <div className="h-[1000px]">
-                            {DataDocument.documentUrl ? (
+                            {DataDocument?.documentUrl ? (
                                 DataDocument.fileType === "pdf" ? (
-                                    <iframe src={DataDocument?.documentUrl} width="100%" height="100%" />
+                                    <iframe src={DataDocument.documentUrl} width="100%" height="100%" />
                                 ) : (
                                     <Button
-                                        onClick={() => saveAs(DataDocument?.documentUrl, "document-file")}
+                                        onClick={() => saveAs(DataDocument.documentUrl, "document-file")}
                                         className="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-300 ease-in-out"
                                     >
                                         Download Document
