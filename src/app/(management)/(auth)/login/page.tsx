1c48bb2250c0b326c5ebe8a6efd81da9eefbf77a
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2025-01-31 08:34:02 +0700

    debuging

--- src/app/(management)/(auth)/login/page.tsx
+++ src/app/(management)/(auth)/login/page.tsx
@@ -65,7 +65,6 @@ export default function LoginForm() {
                 setError(response);
             }
         } catch (err) {
-            console.log(err);
             setError("Login failed. Please check your credentials.");
 
         } finally {
