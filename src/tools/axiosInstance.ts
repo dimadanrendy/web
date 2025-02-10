93cd34215f4aab4dcb38b536b5b255aed68170ef
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2025-02-10 17:52:01 +0700

    Link PBB

--- src/tools/axiosInstance.ts
+++ src/tools/axiosInstance.ts
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL: process.env.NEXT_API_URL,
   withCredentials: true,
   headers: {
-    "x-secret-key": `${process.env.NEXT_PUBLIC_SECRET_KEY}`,
+    "x-secret-key": `${process.env.PRIVATE_KEY}`,
   },
 });
 
