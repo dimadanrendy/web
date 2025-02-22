bc1c34c7730d1095154bcddd4c0b392774d7db1b
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2025-02-22 12:08:35 +0700

    Debug

--- src/features/management/auth/useLoginAuth.ts
+++ src/features/management/auth/useLoginAuth.ts
@@ -1,12 +1,8 @@
 import axiosInstance from "@/tools/axiosInstance";
-import axios from "axios";
 
 export const login = async (data: any) => {
   try {
-    const response = await axios.post(
-      "https://bakeuda.pangkalpinangkota.go.id/api/v1/auth",
-      data
-    );
+    const response = await axiosInstance.post("/auth", data);
     return response.data;
   } catch (error: any) {
     return error.response.data.message;
