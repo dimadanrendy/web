72b440dd1b5976c7d7ff97268f623fcdb610f4cc
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2025-02-22 12:29:04 +0700

    Debug

--- src/tools/axiosInstance.ts
+++ src/tools/axiosInstance.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
   headers: {
-    "x-secret-key": `${process.env.PRIVATE_KEY}`,
+    "x-secret-key": `${process.env.NEXT_PUBLIC_SECRET_KEY}`,
   },
 });
 
